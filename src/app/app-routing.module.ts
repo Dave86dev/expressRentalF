aae8fc21aa9d20ef2d147dd41cfb476e47837ab2
Dave86 <davidochando1986@gmail.com>
2019-11-19 21:38:50 +0100

    FrontEnd framework Initialization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { MovieDisplayComponent } from './components/movie-display/movie-display.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo: "display", pathMatch:"full"},
+  {path:"display", component:MovieDisplayComponent},
+  {path:"login", component:LoginComponent},
+  {path:"register", component:RegisterComponent},
+  {path:"movie:/id", component:MovieDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { MovieDisplayComponent } from './components/movie-display/movie-display.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
+import { RegisterComponent } from './components/register/register.component';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+//import {MatInputModule, MatButtonModule} from '@angular/material';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    MovieDisplayComponent,
+    MovieDetailComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    //MatInputModule,
+    //MatButtonModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
